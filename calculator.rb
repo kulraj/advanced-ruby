15a59dd91c8ebb26b7fa64a1f88dce6e302548cf
kulraj <kulraj@vinsol.com>
2013-08-08 15:01:58 +0530

    code optimized

--- calculator.rb
+++ calculator.rb
@@ -21,29 +21,14 @@ class String
     end
     operation_array = split(" ")
     # validate the two operands
-    operand1 = operation_array[0].validate_number.to_f
-    operand2 = operation_array[2].validate_number.to_f
+    operation_array[0].validate_number
+    operation_array[2].validate_number
     # the operator is at index 1
-    case operation_array[1]
-    when '+'
-      result = operand1 + operand2
-    when '-'
-      result = operand1 - operand2
-    when '*'
-      result = operand1 * operand2
-    when '/'
-      result = operand1 / operand2
-    else
-      puts "Wrong input for operator"
-      exit
-    end
-    # return the result of the operation
-    result
-    end
+    eval(self)
   end
 end
 
 puts "Enter the operation (operand1 operator operand2)\nKeep spaces to separate the operands and operator"
 operation = gets.chomp
 result = operation.calculate
-puts "#{operand1} #{operation_array[1]} #{operand2} = #{result}"
+puts result
