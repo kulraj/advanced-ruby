7e48ca362fa8707f0c638cde14f79751d123332e
kulraj <kulraj@vinsol.com>
2013-08-09 16:58:49 +0530

    method added and defined

--- method_command_line.rb
+++ method_command_line.rb
@@ -1,8 +1,13 @@
 #Prompt the user for a method name and a single line of code; then actually define the method and call it
 
-class String
-  def method(*args)
-    eval args[0]
+class Prompt
+  def initialize(method_name)
+    @method_name = method_name
+  end
+  def addMethod( method_name, &block )
+    self.class.send( :define_method, method_name , &block )
+  end
+  define_method("#{@method_name}") do |&block|
   end
 end
 
@@ -10,4 +15,6 @@ print "Enter the method name: "
 method_name = gets.chomp
 puts "Enter a line of code:"
 line_code = gets
-method_name.send(:method, line_code)
+prompt = Prompt.new(method_name)
+prompt.addMethod(method_name) { eval "#{line_code}" }
+prompt.send(method_name)
