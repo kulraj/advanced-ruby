745bc56359beb16069a6edce06e329886a98a623
kulraj <kulraj@vinsol.com>
2013-08-09 12:48:57 +0530

    dynamic method rectified

--- method_command_line.rb
+++ method_command_line.rb
@@ -1,14 +1,13 @@
 #Prompt the user for a method name and a single line of code; then actually define the method and call it
 
-class PromptMethod
-  def method_missing(method_name, *args)
+class String
+  def method(*args)
     eval args[0]
   end
 end
 
-print "Enter method name: "
+print "Enter the method name: "
 method_name = gets.chomp
-puts "Enter line of code"
+puts "Enter a line of code:"
 line_code = gets
-prompt_object = PromptMethod.new
-prompt_object.method_name(line_code)
+method_name.send(:method, line_code)
