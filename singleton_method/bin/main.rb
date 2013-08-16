27824dabd7fa0cf5611d2aa9a4759f4d18c5fa22
kulraj <kulraj@vinsol.com>
2013-08-16 18:24:28 +0530

    indentation rectified

--- singleton_method/bin/main.rb
+++ singleton_method/bin/main.rb
@@ -5,21 +5,19 @@ If you create another instance of the class and try to call this method, it shou
 Create this method using def something.method_name and also class << self	
 =end
 
-require_relative "../lib/string"
-
 string_instance = "abcd"
 #method1
 def string_instance.method1
-	puts self
+  puts self
 end
-string_instance.method
+string_instance.method1
 
 #method2
 string_instance.instance_eval do
-	class << self
-		def method2
-			puts swapcase
-		end
-	end
+  class << self
+    def method2
+      puts swapcase
+    end
+  end
 end
 string_instance.method2
