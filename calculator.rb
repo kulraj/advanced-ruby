08ef0bea77c7f2024a7b8052b9b7d522a4fcb1de
kulraj <kulraj@vinsol.com>
2013-08-21 11:18:21 +0530

    done through send

--- calculator.rb
+++ calculator.rb
@@ -11,24 +11,29 @@ class String
       puts "Input is not a number"
       exit
     end
-    self
+    self.to_f
   end
 
-  def calculate
+  def validate_format
     if !match(/.+\s.\s.+/)
       puts "Invalid format entered"
       exit
     end
+  end
+
+  def calculate
     operation_array = split(" ")
     # validate the two operands
-    operation_array[0].validate_number
-    operation_array[2].validate_number
+    operand1 = operation_array[0].validate_number
+    operand2 = operation_array[2].validate_number
     # the operator is at index 1
-    eval(self)
+    operand1.send(operation_array[1].to_sym, operand2).round(2)
   end
+
 end
 
 puts "Enter the operation (operand1 operator operand2)\nKeep spaces to separate the operands and operator"
 operation = gets.chomp
+operation.validate_format
 result = operation.calculate
 puts result
